aa9dfabac1f1113dc9b869be37a522bf5e5b872c
bigyax <soleary80@gmail.com>
2011-03-06 15:02:10 +0000

    Fetching results for two drives, grouping and printing on same file size

--- TeraDupeConsole/Program.cs
+++ TeraDupeConsole/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Recls;
 
 namespace TeraDupeConsole
 {
@@ -9,6 +10,28 @@ namespace TeraDupeConsole
     {
         static void Main(string[] args)
         {
+            var query1 = from file in FileSearcher.Search(@"I:\", "*.avi")
+                         select file;
+
+            var query2 = from file in FileSearcher.Search(@"K:\", "*.avi")
+                         select file;
+
+            var concat = query1.Concat(query2);
+
+            var fileGroups =
+                from file in concat
+                group file by file.Size
+                into groupedFiles
+                where groupedFiles.Count() > 1
+                select groupedFiles;
+
+            foreach (var fileGroup in fileGroups)
+            {
+                foreach (var file in fileGroup)
+                {
+                    Console.WriteLine(file.FileName + " : " + file.Size);
+                }
+            }
         }
     }
 }
