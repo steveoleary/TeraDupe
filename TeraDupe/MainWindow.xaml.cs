1aee6076cde6af70d45542183ec3cbd2529e5859
bigyax <soleary80@gmail.com>
2011-03-07 19:42:08 +0000

    Added FolderBrowserDialogEx.cs

--- TeraDupe/MainWindow.xaml.cs
+++ TeraDupe/MainWindow.xaml.cs
@@ -22,6 +22,35 @@ namespace TeraDupe
         public MainWindow()
         {
             InitializeComponent();
+
+
         }
+
+        string _folderName = "c:\\dinoch";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _folderName = (System.IO.Directory.Exists(_folderName)) ? _folderName : "";
+            var dlg1 = new Ionic.Utils.FolderBrowserDialogEx
+            {
+                Description = "Select a folder for the extracted files:",
+                ShowNewFolderButton = true,
+                ShowEditBox = true,
+                //NewStyle = false,
+                SelectedPath = _folderName,
+                ShowFullPathInEditBox = false,
+            };
+            dlg1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+
+            var result = dlg1.ShowDialog();
+
+            //if (result == DialogResult.)
+            //{
+                _folderName = dlg1.SelectedPath;
+                //this.label1.Text = "The folder selected was: ";
+                //this.label2.Text = _folderName;
+           // }
+        }
+        
+
     }
 }
