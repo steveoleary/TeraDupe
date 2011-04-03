93bb6edf20bd9396ce65b019157edcae70723fdb
bigyax <soleary80@gmail.com>
2011-04-03 20:10:59 +0100

    Added path binding to listbox

--- TeraDupe/MainWindow.xaml.cs
+++ TeraDupe/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -11,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Recls;
 
 namespace TeraDupe
 {
@@ -19,14 +22,23 @@ namespace TeraDupe
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<PathToSearch> paths = new ObservableCollection<PathToSearch>();
+
         public MainWindow()
         {
             InitializeComponent();
 
-
+            lbPathsToSearch.ItemsSource = paths;
         }
 
-        string _folderName = "c:\\dinoch";
+        string _folderName = "";
+
+        public ObservableCollection<PathToSearch> Paths
+        {
+            get { return paths; }
+            set { paths = value; }
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             _folderName = (System.IO.Directory.Exists(_folderName)) ? _folderName : "";
@@ -35,24 +47,86 @@ namespace TeraDupe
                 Description = "Select a folder for the extracted files:",
                 ShowNewFolderButton = true,
                 ShowEditBox = true,
-                //NewStyle = false,
                 SelectedPath = _folderName,
                 ShowFullPathInEditBox = false,
             };
             dlg1.RootFolder = System.Environment.SpecialFolder.MyComputer;
 
             var result = dlg1.ShowDialog();
-            listView1.Items.Add(dlg1.SelectedPath);
-
-
-            //if (result == DialogResult.)
-            //{
-                _folderName = dlg1.SelectedPath;
-                //this.label1.Text = "The folder selected was: ";
-                //this.label2.Text = _folderName;
-           // }
+            paths.Add(new PathToSearch(dlg1.SelectedPath, true));
         }
-        
 
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            var query1 = from file in FileSearcher.Search(@"I:\", "*.avi")
+                         select file;
+
+            var query2 = from file in FileSearcher.Search(@"K:\", "*.avi")
+                         select file;
+
+            var concat = query1.Concat(query2);
+
+            var fileGroups =
+                from file in concat
+                group file by new { file.Size, hash = GetHash(file) }
+                    into groupedFiles
+                    where groupedFiles.Count() > 1
+                    select groupedFiles;
+
+
+
+            foreach (var fileGroup in fileGroups)
+            {
+                foreach (var file in fileGroup)
+                {
+                    Console.WriteLine(file.FileName + " : " + file.Size);
+                }
+            }
+        }
+
+        private static ulong GetHash(IEntry file)
+        {
+            ulong hashFromBytes;
+
+            using (BinaryReader b = new BinaryReader(File.Open(file.Path, FileMode.Open)))
+            {
+                int length = (int)b.BaseStream.Length;
+                int pos = length / 2;
+                //TODO: If length is less than 2000 bytes read in entire file
+                b.BaseStream.Seek(pos, SeekOrigin.Begin);
+                hashFromBytes = GetHashFromBytes(b.ReadBytes(2000));
+            }
+
+            return hashFromBytes;
+        }
+
+        private static ulong GetHashFromBytes(byte[] b)
+        {
+            ulong result = 0;
+            if ((b != null) || (b.Length != 0))
+            {
+                unchecked
+                {
+                    result = 0xcbf29ce484222325u; //init prime
+                    for (int i = 0; i < b.Length; i++)
+                    {
+                        result = ((result ^ b[i]) * 0x100000001b3); //xor with new and mul with prime
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+    public class PathToSearch
+    {
+        public string SelectedPath { get; set; }
+        public bool SearchRecursively { get; set; }       
+
+        public PathToSearch(string selectedPath, bool searchRecursively)
+        {
+            SelectedPath = selectedPath;
+            SearchRecursively = searchRecursively;
+        }
     }
 }
